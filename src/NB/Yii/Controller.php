be1e89baade4a6dfcf2fd37cf173efd3a49786d2
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2015-10-23 12:56:01 +0200

    Convenience logging

---
+++ src/NB/Yii/Controller.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace NB\Yii;
+
+use Yii;
+use yii\web\Controller as YiiWebController;
+
+/**
+ * Base controller with some convenience features.
+ *
+ * @author Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPLv3
+ */
+abstract class Controller extends YiiWebController
+{
+    abstract protected function getLoggingCategory();
+
+    /**
+     * Logs an error message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function error($message)
+    {
+        Yii::error($message, $this->getLoggingCategory());
+    }
+
+    /**
+     * Logs an informative message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function info($message)
+    {
+        Yii::info($message, $this->getLoggingCategory());
+    }
+
+    /**
+     * Logs a debug message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function trace($message)
+    {
+        Yii::trace($message, $this->getLoggingCategory());
+    }
+
+    /**
+     * Logs an warning message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function warning($message)
+    {
+        Yii::warning($message, $this->getLoggingCategory());
+    }
+}
