5ab5c4926a9a390b1784325a10822646fe55d589
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2015-10-26 15:59:08 +0100

    Lib-Update

--- src/NB/Yii/IDebuggable.php
+++ src/NB/Yii/IDebuggable.php
@@ -15,26 +15,26 @@ class IDebuggable
      *
      * @param string $message The message to log.
      */
-    function error($message);
+    public function error($message);
 
     /**
      * Logs an informative message.
      *
      * @param string $message The message to log.
      */
-    function info($message);
+    public function info($message);
 
     /**
      * Logs a debug message.
      *
      * @param string $message The message to log.
      */
-    function trace($message);
+    public function trace($message);
 
     /**
      * Logs an warning message.
      *
      * @param string $message The message to log.
      */
-    function warning($message);
+    public function warning($message);
 }
