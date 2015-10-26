06d196ad4d56134f209babddda069cfc2ce98a30
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2015-10-26 14:49:34 +0100

    Abstract base model class.

---
+++ src/NB/Yii/Model.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace NB\Yii;
+
+use NB\IDebuggable;
+
+/**
+ * Abstract base model with some convenience features.
+ *
+ * @author Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPLv3
+ */
+abstract class Model implements IDebuggable
+{
+    abstract protected function getLoggingCategory();
+
+    /**
+     * Logs an error message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function error($message)
+    {
+        Yii::error($message, $this->getLoggingCategory());
+    }
+
+    /**
+     * Logs an informative message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function info($message)
+    {
+        Yii::info($message, $this->getLoggingCategory());
+    }
+
+    /**
+     * Logs a debug message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function trace($message)
+    {
+        Yii::trace($message, $this->getLoggingCategory());
+    }
+
+    /**
+     * Logs an warning message.
+     *
+     * @param string $message The message to log.
+     */
+    protected function warning($message)
+    {
+        Yii::warning($message, $this->getLoggingCategory());
+    }
+}
