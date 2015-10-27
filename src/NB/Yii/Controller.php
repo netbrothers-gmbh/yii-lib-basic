86d48a8c06b46f80cb052e9670909943cdff8faf
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2015-10-27 13:49:36 +0100

    Trying to outsource debugging functionality in trait.

--- src/NB/Yii/Controller.php
+++ src/NB/Yii/Controller.php
@@ -2,8 +2,9 @@
 
 namespace NB\Yii;
 
-use Yii;
+//use Yii;
 use yii\web\Controller as YiiWebController;
+use NB\Yii\Traits\DebuggingTrait;
 
 /**
  * Abstract base controller with some convenience features.
@@ -15,43 +16,5 @@ abstract class Controller extends YiiWebController
 {
     abstract protected function getLoggingCategory();
 
-    /**
-     * Logs an error message.
-     *
-     * @param string $message The message to log.
-     */
-    protected function error($message)
-    {
-        Yii::error($message, $this->getLoggingCategory());
-    }
-
-    /**
-     * Logs an informative message.
-     *
-     * @param string $message The message to log.
-     */
-    protected function info($message)
-    {
-        Yii::info($message, $this->getLoggingCategory());
-    }
-
-    /**
-     * Logs a debug message.
-     *
-     * @param string $message The message to log.
-     */
-    protected function trace($message)
-    {
-        Yii::trace($message, $this->getLoggingCategory());
-    }
-
-    /**
-     * Logs an warning message.
-     *
-     * @param string $message The message to log.
-     */
-    protected function warning($message)
-    {
-        Yii::warning($message, $this->getLoggingCategory());
-    }
+    use DebuggingTrait;
 }
