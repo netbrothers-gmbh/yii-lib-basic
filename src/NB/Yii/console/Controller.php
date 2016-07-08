628086889094aa678199297b8b97da1aae0ec1fb
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2016-07-08 15:35:37 +0200

    Console version of the abstract controller

---
+++ src/NB/Yii/console/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NB\Yii;
+
+use NB\Yii\Traits\DebuggingTrait;
+use yii\console\Controller as YiiConsoleController;
+
+/**
+ * Abstract base console controller with some convenience features.
+ *
+ * @author Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPLv3
+ */
+abstract class Controller extends YiiConsoleController
+{
+    /**
+     * Specifies a dynamic logging category for the impementing class. It may
+     * choose to return an identifier based on the class and/or the executed
+     * function.
+     *
+     * @return string The logging category.
+     */
+    abstract protected function getLoggingCategory();
+
+    use DebuggingTrait;
+}
