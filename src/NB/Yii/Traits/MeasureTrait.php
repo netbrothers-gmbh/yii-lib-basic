4e3129a5bcf19b719c962abb66b978813322c3a6
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2016-07-11 14:27:20 +0200

    Outsource performance measuring functionality to trait.

---
+++ src/NB/Yii/Traits/MeasureTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NB\Yii\Traits;
+
+/**
+ * Trait to provide convenient time measuring methods.
+ *
+ * @author Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPLv3
+ */
+trait MeasureTrait
+{
+    /**
+     * @var float Will be set with the current microtime.
+     */
+    private $measureStart;
+
+    /**
+     * Starts the stop watch.
+     */
+    protected function measureStart()
+    {
+        $this->measureStart = microtime(TRUE);
+    }
+
+    /**
+     * @return float The elapsed time since the start of the stop watch.
+     */
+    protected function timeElapsed()
+    {
+        return microtime(TRUE) - $this->measureStart;
+    }
+}
